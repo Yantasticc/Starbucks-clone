217210bac5edee628334543116f026a656240c58
Yantasticc <yashkalamkhede2002@gmail.com>
2023-09-08 23:26:55 +0530

    tailwind working -new config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -6,8 +7,6 @@ function App() {
       <>
          <NavBar />  
       </>
-     
-
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,57 @@
-import React from 'react'
+// import React from 'react'
 import './NavBar.css'
+
+// const NavBar = () => {
+//   return (
+//     <>
+//         <div className='navbar__items'>
+//             <div className='navbar__item'>
+//                 <div>
+//                     <img 
+//                         className='navbar__logo pl-20 pr-20 mt-5' 
+//                         src={require("../images/logo.png")} 
+//                         alt='logo'
+                        
+//                     />
+//                 </div>
+//                 <div className='navbar__options'>
+//                     <span className='navbar__option__green'>Home</span>
+//                     <span className='navbar__option'>Gift</span>
+//                     <span className='navbar__option'>Order</span>
+//                     <span className='navbar__option'>Pay</span>
+//                     <span className='navbar__option'>Store</span>
+//                 </div>
+//             </div>
+//             <div></div>
+//         </div>
+//     </>
+//   )
+// }
+
+// export default NavBar
+
+import React from 'react'
 
 const NavBar = () => {
   return (
     <>
-        <div className='navbar__items'>
-            <div className='navbar__item'>
+        <div className='pl-20 pr-20 mt-5'>
+            <div className='navbar__item flex item center'>
                 <div>
                     <img 
-                        className='navbar__logo' 
                         src={require("../images/logo.png")} 
                         alt='logo'
+                        width={40}     
+                        height={40}
+                        className='cursor-pointer'
                     />
                 </div>
-                <div className='navbar__options'>
-                    <span className='navbar__option'>Home</span>
-                    <span className='navbar__option'>Gift</span>
-                    <span className='navbar__option'>Order</span>
-                    <span className='navbar__option'>Pay</span>
-                    <span className='navbar__option'>Store</span>
+                <div>
+                    <span className=''>Home</span>
+                    <span className=''>Gift</span>
+                    <span className=''>Order</span>
+                    <span className=''>Pay</span>
+                    <span className=''>Store</span>
                 </div>
             </div>
             <div></div>
@@ -28,3 +61,4 @@ const NavBar = () => {
 }
 
 export default NavBar
+
